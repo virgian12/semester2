6a563ab89badf866e022585341e3260f7d0cc4dc
novanrohman <novanrohman@gmail.com>
2022-04-05 14:05:58 +0700

    :white_check_mark:

--- COLLECTION/Percobaan8.java
+++ COLLECTION/Percobaan8.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
     v.add("D");
     v.add("E");
     v.add("F");
-    System.out.println(" Vector contains : " + aList);
+    System.out.println(" Vector contains : " + v);
 
     // Get Enumeration over Vector
     Enumeration<String> e = v.elements();
