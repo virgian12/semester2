a3aaaff2c8c4824893563eb99a7e96e0fd1e3000
virgian12 <102939714+virgian12@users.noreply.github.com>
2022-04-13 07:59:55 +0700

    Add files via upload

---
+++ GENERICS/Percobaan4B.java
@@ -0,0 +1,14 @@
+public class Percobaan4B { 
+    public static void printList(List<?> list) { 
+    for (Object elem : list) { 
+    System.out.print(elem + " "); 
+    } 
+    System.out.println(); 
+    } 
+    public static void main(String[] args) { 
+    List<Integer> li = Arrays.asList(1, 2, 3); 
+    List<String> ls = Arrays.asList("one", "two", "three"); 
+    printList(li); 
+    printList(ls); 
+    } 
+   } 
